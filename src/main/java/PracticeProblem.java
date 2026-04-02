419e8842361b633c906262563151fc71c828ea46
Akshayen Jeyamaran <akshayen.jeyamaran@student.tdsb.on.ca>
2026-04-02 18:40:17 +0000

    Finish Question 1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,17 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
-
+	public static int towerOfHanoi(int num) {
+		if (num < 3) {
+			return -1;
+		}
+		return moves(num);
+	}
+	private static int moves(int num) {
+		if (num == 1) {
+			return 1;
+		}
+		return 2 * moves(num - 1) + 1;
+	}
 }
